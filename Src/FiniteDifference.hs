188725c58a0d01d60c5675acf1899ffe34cc2a0a
Will Dicharry <wdicharry@gmail.com>
2015-07-22 20:24:39 -0600

    Begin implementation of imapStencil function.

--- Src/FiniteDifference.hs
+++ Src/FiniteDifference.hs
@@ -13,6 +13,12 @@ type UpdateFunction = (Discretization -> NumericArray -> Int -> Double)
 
 nextState :: Discretization -> UpdateFunction -> NumericArray -> NumericArray
 nextState d f currentState = V.imap updateElement (V.slice 1 elementCount currentState)
-    where 
+    where
         updateElement = \e _ -> f d currentState e
         elementCount = V.length currentState - 2
+
+imapStencil :: V.Vector Bool -> (Int -> a -> b) -> V.Vector a -> V.Vector b
+imapStencil stencil f v = V.imap conditionF V.zip stencil v where
+    conditionF i (s, value)
+        | s = f i value
+        | otherwise = value
