7bcb01b5f408bc0339f7233f95de86612ff922ea
Will Dicharry <wdicharry@gmail.com>
2015-07-26 14:11:26 -0600

    Output to file.

--- Src/Hsfd.hs
+++ Src/Hsfd.hs
@@ -7,22 +7,26 @@ import Control.Monad
 import qualified Data.Vector as V
 import Data.List
 
+import System.IO
+
 main :: IO ()
 main = do
     let state = testState
-    let steps = [0.0, 0.001 .. 20.0] :: [Double]
+    let steps = [0.0, 0.001 .. 1.0] :: [Double]
     let d = Discretization 0.001 0.05
-    evolve d steps state
+    output <- openFile "out.dat" WriteMode
+    evolve output d steps state
+    hClose output
 
 {- Evolve the system, outputting as we go.
 It seems like I should be able to use forM to evolve the system.
 -}
-evolve :: Discretization -> [Double] -> NumericArray -> IO ()
-evolve _ [] finalState = print $ format finalState
-evolve discretization steps state = do
-    print $ format state
+evolve :: Handle -> Discretization -> [Double] -> NumericArray -> IO ()
+evolve out _ [] finalState = hPutStrLn out $ format finalState
+evolve out discretization steps state = do
+    hPutStrLn out $ format state
     let state' = nextState discretization heatOperator state
-    evolve discretization (tail steps) state'
+    evolve out discretization (tail steps) state'
 
 format :: Show a => V.Vector a -> String
 format v = intercalate " " (map show (V.toList v))
