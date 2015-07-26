c6ad07485d52f974944be2a7796eaa722f819790
Will Dicharry <wdicharry@gmail.com>
2015-07-25 19:45:51 -0600

    Add comments.

--- Src/FiniteDifference.hs
+++ Src/FiniteDifference.hs
@@ -11,9 +11,13 @@ type NumericArray = V.Vector Double
 
 type UpdateFunction = (Discretization -> NumericArray -> Int -> Double -> Double)
 
+{-  Get the next state of the system given the current state. -}
 nextState :: Discretization -> UpdateFunction -> NumericArray -> NumericArray
 nextState d f currentState = imapStencil boundaryStencil (f d currentState) currentState
 
+{- Apply a function to a vector given a stencil. Where the stencil
+evaluates to True, the function will be applied. Where it is False the
+identity is applied. -}
 imapStencil :: V.Vector Bool -> (Int -> a -> a) -> V.Vector a -> V.Vector a
 imapStencil stencil f v = V.imap conditionF (V.zip stencil v) where
     conditionF i (s, value)
