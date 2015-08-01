9ed7f9cac0be3fcff407c05bd40e8e46fd4775b5
Will Dicharry <wdicharry@gmail.com>
2015-08-01 08:40:19 -0600

    Separate time evolution from IO (mostly) and move into pure module.

--- Src/FiniteDifference.hs
+++ Src/FiniteDifference.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
 module FiniteDifference where
 
 import qualified Data.Vector as V
@@ -11,13 +14,13 @@ type NumericArray = V.Vector Double
 
 type UpdateFunction = (Discretization -> NumericArray -> Int -> Double -> Double)
 
-{-  Get the next state of the system given the current state. -}
+{-|  Get the next state of the system given the current state. -}
 nextState :: Discretization -> UpdateFunction -> NumericArray -> NumericArray
 nextState d f state = imapStencil boundaryStencil update identity state where
     update = f d state
     identity _ v = v
 
-{- Apply a function to a vector given a stencil. Where the stencil
+{-| Apply a function to a vector given a stencil. Where the stencil
 evaluates to True, the function will be applied. Where it is False the
 falseFunction is applied. -}
 imapStencil :: V.Vector Bool -> (Int -> a -> b) -> (Int -> a -> b)
@@ -29,3 +32,22 @@ imapStencil stencil t f v = V.imap conditional zipped where
         | otherwise = f i value
 
 boundaryStencil = V.singleton False V.++ V.replicate 18 True V.++ V.singleton False
+
+{-| Allow customization of vector IO.
+FIXME I don't like having to put the type constraint on writeVector, I think
+there's a way to allow the type constraint to be declared on the instance
+(which is what would make sense because there's no reason to require Show in
+the typeclass definition).
+-}
+class Monad m => VectorIO h m | m -> h where
+    writeVector :: Show a => h -> V.Vector a -> m ()
+
+{-| Evolve the given operator through a set of timesteps, processing each state
+using a VectorIO instance.
+-}
+evolve :: VectorIO h m => h -> Discretization -> [Double] -> UpdateFunction -> NumericArray -> m ()
+evolve out _ [] _ finalState = writeVector out finalState
+evolve out discretization steps operator state = do
+    writeVector out state
+    evolve out discretization (tail steps) operator state'
+        where state' = nextState discretization operator state
--- Src/Hsfd.hs
+++ Src/Hsfd.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 import System.Environment
 
-import FiniteDifference(Discretization(..), NumericArray, nextState)
+import FiniteDifference(
+    Discretization(..), NumericArray, nextState, VectorIO, writeVector, evolve)
 import HeatEquation(heatOperator, testState)
 
 import Control.Monad
@@ -15,20 +17,11 @@ main = do
     let steps = [0.0, 0.001 .. 1.0] :: [Double]
     let d = Discretization 0.001 0.05
     output <- openFile "out.dat" WriteMode
-    evolve output d steps state
+    evolve output d steps heatOperator state
     hClose output
 
-{- Evolve the system, outputting as we go.
-    FIXME:
-    It seems like I should be able to use forM to evolve the system.
-    It also seems like I shouldn't have to mix evolution with IO
--}
-evolve :: Handle -> Discretization -> [Double] -> NumericArray -> IO ()
-evolve out _ [] finalState = hPutStrLn out $ format finalState
-evolve out discretization steps state = do
-    hPutStrLn out $ format state
-    let state' = nextState discretization heatOperator state
-    evolve out discretization (tail steps) state'
+instance VectorIO Handle IO where
+    writeVector handle vector = hPutStrLn handle $ format vector
 
 format :: Show a => V.Vector a -> String
 format = unwords . (map show) . V.toList
